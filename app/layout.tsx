9b826521f833cf5a0b713a75b8f86be4322507af
hetkeraliya <jalpa5398@gmail.com>
2026-02-22 16:45:09 +0530

    Move Inter font import to a new line

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { Home, CheckSquare, Calendar, User } from "lucide-react";
 import Link from "next/link";
 
