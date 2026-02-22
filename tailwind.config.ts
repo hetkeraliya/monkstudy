284fe172bf5feb630b9e8dc29750baad6fc84474
hetkeraliya <jalpa5398@gmail.com>
2026-02-22 16:33:07 +0530

    Update content paths in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -34,4 +35,3 @@ const config: Config = {
   plugins: [],
 };
 export default config;
-
