4b5dccfbbe4eaa06cbf2d90483030e20bf4a50e9
hetkeraliya <jalpa5398@gmail.com>
2026-02-27 19:05:25 +0530

    Update AppShell.tsx

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -1,27 +1,16 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
-import AuthGuard from "./AuthGuard";
 
 export default function AppShell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  const isAuthPage =
-    pathname === "/login" || pathname === "/register";
-
-  if (isAuthPage) {
-    return <>{children}</>;
-  }
-
   return (
-    <AuthGuard>
+    <>
       {children}
       <Navbar />
-    </AuthGuard>
+    </>
   );
 }
